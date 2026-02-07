501d29c7b938160440feff30c98ba5987bd18810
NatBiscarra <natbiscarra@gmail.com>
2026-02-07 17:26:27 -0300

    Add Book CRUD with user association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_07_185955) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_07_200010) do
+  create_table "books", force: :cascade do |t|
+    t.string "author"
+    t.datetime "created_at", null: false
+    t.string "title"
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.integer "year"
+    t.index ["user_id"], name: "index_books_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
@@ -22,4 +32,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_07_185955) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "books", "users"
 end
