800ae09d142c6d3b6a084ad73faacfe79adc391c
NatBiscarra <natbiscarra@gmail.com>
2026-02-10 17:04:57 -0300

    add Bootstrap for UI styling

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,4 +5,8 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+
 pin "books_search"
+
+pin "bootstrap", to: "bootstrap.js" # @5.3.8
+pin "@popperjs/core", to: "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/esm/index.js" # @2.11.8 (use CDN ESM bundle)
